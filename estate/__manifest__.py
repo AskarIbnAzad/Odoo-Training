038633a7578bcc993ca17ddab35a464b4d3c2cf7
Askar223 <askaribn223@gmail.com>
2025-10-12 15:46:46 +0600

    [Add] estate : Add Configuration File : Odd_Learning

---
+++ estate/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Real Estate Askar',
+    'version': '18.0.1.0.0',
+    'category': 'Website/eCommerce',
+    'summary': 'Adds a "Go To Checkout" button to the cart notification popup.',
+    'author': 'Odoo Flash Pro',
+    'depends': [
+        'base',
+    ],
+
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
