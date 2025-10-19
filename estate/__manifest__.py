8558899c3a1879e6bc4c3cbb5f4dbcd90503d7af
Askar223 <askaribn223@gmail.com>
2025-10-19 23:12:57 +0600

    [Edit] estate : Move Access File To Security Folder : Odd_Learning

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,7 +7,9 @@
     'depends': [
         'base',
     ],
-
+    'data':[
+        'security/ir.model.access.csv'
+    ],
     'installable': True,
     'application': False,
     'auto_install': False,
